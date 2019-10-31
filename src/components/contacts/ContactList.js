ff2191dcffed6fd3b7848f33a5ed6f1cb4cd6c95
annelise <al.vanhoegaerden@gmail.com>
2019-10-31 17:08:03 +0100

    contact list html tags + css changes. In progress.

--- src/components/contacts/ContactList.js
+++ src/components/contacts/ContactList.js
@@ -3,34 +3,29 @@ import './ContactList.scss';
 
 const ContactList = () => (
   <React.Fragment>
-    <div>
-      <h1 className="title is-1 mbmd">Contacts utiles .</h1>
-    </div>
-    <div>
-      <div className="row">
-        <div className="photo">
-          <img
+    <h1 className="title is-1 mbmd">Contacts utiles</h1>
+    <div className="row">
+      <section className="photo">
+        {/* <img
             src="http://blogue-ton-ecole.ac-dijon.fr/wp-content/uploads/2016/07/Avatar_girl_face.png"
             className="image is-48x48 is-rounded avatar"
             alt="Un contact"
-          />
-        </div>
-        <div className="textBox">
-          <p>TEXT box</p>
-          <p>Descriptif de la personne et contacts reseaux / autres</p>
-          <p className="description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                      Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                      laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure  enim ad minim veniam,
-                      quis nostrud exercitation ullamco laboris.
-                      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                      commodo consequat.
-          {' '}
-          </p>
-        </div>
-      </div>
+          /> */}
+      </section>
+      <section className="textBox">
+        <h1>TEXT box</h1>
+        <h2>Descriptif de la personne et contacts reseaux / autres</h2>
+        <p className="description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure  enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat.
+        </p>
+      </section>
     </div>
   </React.Fragment>
 );
