1c17fe5c75e8421b46cd95611ec8419483bf9058
messaoy <youcef.messao@gmail.com>
2019-11-06 21:19:37 +0100

    search bar step 3 promo creation

--- src/components/promoCreateForm/PromoCreateStepThree.js
+++ src/components/promoCreateForm/PromoCreateStepThree.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
 import './PromoCreate.scss';
 import { BulmaSteps } from '../bulma-steps/BulmaSteps';
+import SearchbarAutoComplete from '../searchbarauto/SearchbarAuto';
+
+const formers = [
+  {
+    label: 'Jérémie Patonnier',
+    value: 'Jérémie Patonnier',
+  },
+  {
+    label: 'Jonathan Barthelemy',
+    value: 'Jonathan Barthelemy',
+
+  },
+  {
+    label: 'Monsieur X',
+    value: 'Monsieur X',
+  },
+  {
+    label: 'Madame Y',
+    value: 'Madame Y',
+  },
+];
+
+const students = [
+  {
+    label: 'Jeremy Pluquet',
+    value: 'Jeremy Pluquet',
+  },
+  {
+    label: 'Youcef Messaoudene',
+    value: 'Youcef Messaoudene',
+
+  },
+  {
+    label: 'Anne-Lise Vanhoegaerden',
+    value: 'Monsieur X',
+  },
+  {
+    label: 'Claudine Lafine',
+    value: 'Claudine Lafine',
+  },
+];
+
 export class PromoCreateStepThree extends Component {
   render() {
-    const { nextStep, prevStep, isTeachers, step } = this.props;
+    const {
+      nextStep, prevStep, isTeachers, step,
+    } = this.props;
     const buttonForm = (
       <section className="field buttonField">
         <section className="control">
@@ -21,15 +65,15 @@ export class PromoCreateStepThree extends Component {
           <h1 className="title is-4 is-spaced">Création d'une promo</h1>
           <BulmaSteps step={step} />
           <section className="control">
-            <label className="label">Choisir des {isTeachers ? " formateurs " : " élèves "} existants:</label>
+            <label className="label">
+Choisir des
+              {isTeachers ? ' formateurs ' : ' élèves '}
+              {' '}
+existants:
+            </label>
             <section className="field">
               <section className="control">
-                <section className="select">
-                  <select>
-                    <option></option>
-                    <option>{isTeachers ? "Jérémie Patonier" : "Jérémy Pluquet"}</option>
-                  </select>
-                </section>
+                <SearchbarAutoComplete searchObject={isTeachers ? formers : students} searchKey="name" isMulti />
               </section>
             </section>
           </section>
