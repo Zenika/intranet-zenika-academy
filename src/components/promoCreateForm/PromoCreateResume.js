56e9d2d2695933353bb7e98c8f8c2e884ee8280c
D0C702WH0 <jemyplu@gmail.com>
2019-11-07 09:15:41 +0100

    resume step one ok

--- src/components/promoCreateForm/PromoCreateResume.js
+++ src/components/promoCreateForm/PromoCreateResume.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react'
+import { BulmaSteps } from '../bulma-steps/BulmaSteps';
+import './PromoCreate.scss';
+
 
 export class PromoCreateResume extends Component {
-    render() {
-        return (
-            <div>
+  render() {
+    const { step, handleChange, promo, prevStep } = this.props;
+    console.log(promo);
 
-            </div>
-        )
-    }
+
+    const buttonForm = (
+      <section className="field buttonField">
+        <section className="control">
+          <button className="button is-danger" onClick={prevStep}>Revenir</button>
+        </section>
+        <section className="control">
+          <button className="button is-link" >Continuer</button>
+        </section>
+      </section>
+    );
+
+    return (
+      <div className="promoCreateForm">
+        <article className="section box">
+          <h1 className="title is-4 is-spaced">Création d'une promo</h1>
+          <BulmaSteps step={step} />
+          <section className="control">
+            <label className="label">Nom de la promo: </label>
+            <p>{promo.title}</p>
+          </section>
+          <section>
+            <section className="control">
+              <label className="label">Date de début: </label>
+              <time >{promo.startDate}</time>
+            </section>
+          </section>
+          <section>
+            <section className="control">
+              <label className="label">Date de fin: </label>
+              <time >{promo.endDate}</time>
+            </section>
+          </section>
+          <section className="field">
+            <label className="label">Pays:</label>
+            <p>{promo.country}</p>
+          </section>
+          <section className="field">
+            <label className="label">Ville:</label>
+            <section className="field">
+              <p>{promo.city}</p>
+            </section>
+          </section>
+          {buttonForm}
+        </article>
+      </div>
+    )
+  }
 }
 
 export default PromoCreateResume
