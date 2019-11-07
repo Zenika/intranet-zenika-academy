c1425bae5ced8a6689507a4b79964836a907f83a
annelise <al.vanhoegaerden@gmail.com>
2019-11-07 15:21:26 +0100

    add promo filter title + improved design

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import DashboardPromData from './DashboardPromData.js';
-import DashboardShearchbar from './DashboardSearchbar.js';
 import DashboardFilterBar from './DashboardFilterBar.js';
 
 class Dashboard extends React.Component {
     render() {
         return (
             <React.Fragment>
-                    <h1 className="title is-2 mbmd">Dashboard</h1>
-                <div>
+                <h1 className="title is-2 mbmd">Dashboard</h1>
+                <h2 className="title is-4 mbmd">Filtrer les promotions</h2>
                 <DashboardFilterBar />
                 <DashboardPromData />
-                </div>
             </React.Fragment>
         );
     }
