923889264bde435116e642889d70826ee47c94f7
D0C702WH0 <jemyplu@gmail.com>
2019-11-07 09:24:30 +0100

    resume step one refacto

--- src/components/promoCreateForm/PromoCreateResume.js
+++ src/components/promoCreateForm/PromoCreateResume.js
@@ -15,7 +15,7 @@ export class PromoCreateResume extends Component {
           <button className="button is-danger" onClick={prevStep}>Revenir</button>
         </section>
         <section className="control">
-          <button className="button is-link" >Continuer</button>
+          <button className="button is-link" >Valider</button>
         </section>
       </section>
     );
@@ -26,24 +26,38 @@ export class PromoCreateResume extends Component {
           <h1 className="title is-4 is-spaced">Création d'une promo</h1>
           <BulmaSteps step={step} />
           <section className="control">
-            <label className="label">Nom de la promo: </label>
-            <p>{promo.title}</p>
-          </section>
-          <section>
-            <section className="control">
-              <label className="label">Date de début: </label>
-              <time >{promo.startDate}</time>
+            <section className="field">
+              <label className="label">Nom de la promo: </label>
+              <section className="field">
+                <p>{promo.title}</p>
+              </section>
             </section>
           </section>
           <section>
             <section className="control">
-              <label className="label">Date de fin: </label>
-              <time >{promo.endDate}</time>
+              <section className="field">
+                <label className="label">Date de début: </label>
+                <section className="field">
+                  <span> <time >{promo.startDate}</time> </span>
+                </section>
+              </section>
+            </section>
+          </section>
+          <section>
+            <section className="control">
+              <section className="field">
+                <label className="label">Date de fin: </label>
+                <section className="field">
+                  <span> <time >{promo.endDate}</time> </span>
+                </section>
+              </section>
             </section>
           </section>
           <section className="field">
             <label className="label">Pays:</label>
-            <p>{promo.country}</p>
+            <section className="field">
+              <p>{promo.country}</p>
+            </section>
           </section>
           <section className="field">
             <label className="label">Ville:</label>
