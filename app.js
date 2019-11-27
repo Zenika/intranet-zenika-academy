21e7c1c2957c370943acafedc9048597a59080fa
D0C702WH0 <jemyplu@gmail.com>
2019-11-27 16:30:57 +0100

    routers fix

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const promotion = require('./routes/promotion');
+const promotions = require('./routes/promotions');
 const rssFeed = require('./routes/rssFeed');
+const users = require('./routes/users');
+const programs = require('./routes/programs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -10,27 +12,27 @@ app.use(cors());
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
 
-app.use('/api/users', promotion);
-app.use('/api/programs', promotion);
-app.use('/api/promotions', promotion);
+app.use('/api/users', users);
+app.use('/api/programs', programs);
+app.use('/api/promotions', promotions);
 app.use('/api/rssFeed', rssFeed);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = err;
