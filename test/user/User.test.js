36375b0c87405f6fd51ac7ba3e4321838d97361a
D0C702WH0 <jemyplu@gmail.com>
2019-11-28 13:25:49 +0100

    user crud tests admin & student ok

--- test/user/User.test.js
+++ test/user/User.test.js
@@ -3,8 +3,12 @@ const httpMethod = require('../testApiMethodService');
 
 
 describe('Test User Controller Api call', () => {
-  let testUserId = '';
-  let testUserRole = 0;
+  let testAdminUserId = '';
+  let testAdminUserRole = 0;
+
+  let testStudentUserId = '';
+  let testStudentUserRole = 0;
+
   it('Get all Users', async (done) => {
     const response = await httpMethod.get('/api/users').expect(200);
     if (response.body.length > 1) {
@@ -30,8 +34,8 @@ describe('Test User Controller Api call', () => {
       email: 'tatayoyo@gmail.com',
     };
     const response = await httpMethod.post('/api/users', body).expect(201);
-    testUserId = response.body.id;
-    testUserRole = response.body.role;
+    testAdminUserId = response.body.id;
+    testAdminUserRole = response.body.role;
     expect(response.body.id).toEqual(expect.any(Number));
     expect(response.body.firstName).toEqual(body.firstName);
     expect(response.body.lastName).toEqual(body.lastName);
@@ -41,23 +45,62 @@ describe('Test User Controller Api call', () => {
     done();
   });
 
-  it('Update the created user', async (done) => {
+  it('Update the created admin user', async (done) => {
     const bodyUpdate = {
       firstName: 'prénomTestUpdate',
       lastName: 'nomTestUpdate',
-      role: testUserRole,
+      role: testAdminUserRole,
       email: 'tatayoyo@gmail.com',
     };
 
-    const response = await httpMethod.put(`/api/users/${testUserId}/update`, bodyUpdate).expect(200);
+    const response = await httpMethod.put(`/api/users/${testAdminUserId}/update`, bodyUpdate).expect(200);
 
     expect(response.body[0]).toBe(1);
 
     done();
   });
 
-  it('Delete the created user', async (done) => {
-    await httpMethod.delete(`/api/users/${testUserId}`).expect(200);
+  it('Delete the created admin user', async (done) => {
+    await httpMethod.delete(`/api/users/${testAdminUserId}`).expect(200);
+    done();
+  });
+
+  it('Create a new user with student role', async (done) => {
+    const body = {
+      firstName: 'prénomTestPost',
+      lastName: 'nomTestPost',
+      role: 'student',
+      email: 'tatayoyo@gmail.com',
+    };
+    const response = await httpMethod.post('/api/users', body).expect(201);
+    testStudentUserId = response.body.id;
+    testStudentUserRole = response.body.role;
+    expect(response.body.id).toEqual(expect.any(Number));
+    expect(response.body.firstName).toEqual(body.firstName);
+    expect(response.body.lastName).toEqual(body.lastName);
+    expect(response.body.email).toEqual(body.email);
+    expect(response.body.role).toEqual(1);
+    expect(response.body.password).not.toEqual(body.password);
+    done();
+  });
+
+  it('Update the created student user', async (done) => {
+    const bodyUpdate = {
+      firstName: 'prénomTestUpdate',
+      lastName: 'nomTestUpdate',
+      role: testStudentUserRole,
+      email: 'tatayoyo@gmail.com',
+    };
+
+    const response = await httpMethod.put(`/api/users/${testStudentUserId}/update`, bodyUpdate).expect(200);
+
+    expect(response.body[0]).toBe(1);
+
+    done();
+  });
+
+  it('Delete the created student user', async (done) => {
+    await httpMethod.delete(`/api/users/${testStudentUserId}`).expect(200);
     done();
   });
 });
