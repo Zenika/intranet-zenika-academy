412aaf07137865f50b93bb114dd694cb9391baf2
DaYanne <claudine.lafine@gmail.com>
2019-11-29 14:40:40 +0100

    Commit before pulling cleanMVP

--- src/components/navigation/Navigation.test.js
+++ src/components/navigation/Navigation.test.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import TestUtils from 'react-dom/test-utils';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import NavigationBar from './Navigation';
 
+describe('Navbar renders correctly', () => {
+  const navbar = mount(<NavigationBar />);
+
+  beforeEach(() => {
+    navbar.instance();
+  });
+
+  test('it renders correctly', () => {
+    expect(navbar).toMatchSnapshot();
+  });
+  test('Displays the connexion Modal when sign in button is clicked', () => {
+    const siButton = navbar.find('.signInDesktop');
+    siButton.simulate('click');
+
+    expect(navbar).toMatchSnapshot();
+  });
+});
+
 describe('Navigation bar when the user isn\'t connected', () => {
+  const navbar = shallow(<NavigationBar />);
+  const navbarInstance = navbar.instance();
   const nav = <NavigationBar />;
-  test('Navigation bar exists', () => {
+
+  test('Navigation bar displays a sign in button', () => {
     expect(nav).toBeDefined();
     expect(TestUtils.isElement(nav)).toBe(true);
     expect(TestUtils.isElementOfType(nav, NavigationBar)).toBe(true);
@@ -17,12 +37,24 @@ describe('Navigation bar when the user isn\'t connected', () => {
     expect(siButton.textContent).toEqual('Se connecter');
   });
 
-  test('Shallow rendering', () => {
-    const navbar = shallow(<NavigationBar />);
-    const navbarInstance = navbar.instance();
-
+  beforeEach(() => {
     navbarInstance.componentDidMount();
+  });
 
+  // afterEach(() => {
+  //   navbarInstance.unmount();
+  // });
+
+  test('Shallow rendering', () => {
     expect(navbar.state('modalState')).toBe('');
+    expect(navbar.state('loggedIn')).toBe(false);
+  });
+
+  test('State mutates after clicking on sign in button', () => {
+    const siButton = navbar.find('.signInDesktop');
+    siButton.simulate('click');
+
+    expect(navbar.state('modalState')).toBe('is-active');
+    expect(navbar.state('loggedIn')).toBe(false);
   });
 });
