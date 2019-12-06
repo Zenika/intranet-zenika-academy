226fcc4d05097d9e253d67deb15c518cf540ad50
messaoy <youcef.messao@gmail.com>
2019-12-06 10:30:41 +0100

    user form test

--- src/components/user/RecapUserForm.js
+++ src/components/user/RecapUserForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Axios from 'axios';
-import { Link, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import './UserProfile.scss';
 import '../layout/Layout.scss';
 
@@ -13,63 +13,73 @@ class RecapProgramForm extends React.Component {
     };
   }
 
-  createUser = (e) => {
-    e.preventDefault();
-    const { user } = this.state;
-    Axios.post('http://localhost:4000/api/users ', user)
-      .then(() => this.setState(() => ({ redirectToReferrer: true })));
-  };
-
-  render() {
-    const { user } = this.props;
-    let roleDetail;
-    if (user.role === 'admin') roleDetail = 'Administrateur';
-    if (user.role === 'teacher') roleDetail = 'Formateur';
-    if (user.role === 'student') roleDetail = 'Eleve';
-
-    const { redirectToReferrer } = this.state;
-    if (redirectToReferrer === true) {
-      return <Redirect to="/home/admin" />;
-    }
-
-    return (
-      <article className="section box">
-        <h2 className="title is-2 is-spaced">Récapitulatif du formulaire d&lsquo;utilisateur</h2>
-        <section className="box">
-          <h2 className="title is-2 is-spaced">
-            <ul className="list">
-              <li className="list-item">
-                Nom :&nbsp;
-                { user.lastName }
-              </li>
-              <li className="list-item">
-                Prénom :&nbsp;
-                { user.firstName }
-              </li>
-              <li className="list-item">
-                Email :&nbsp;
-                { user.email }
-              </li>
-              <li className="list-item">
-                Rôle :&nbsp;
-                { roleDetail }
-              </li>
-            </ul>
-          </h2>
-        </section>
-        <div className="field is-grouped">
-          <div className="control">
-              <button type="submit" onClick={(e) => this.createUser(e)} className="button is-success">Créer</button>
-          </div>
-          <div className="control">
-            <Link to="/home/admin">
-              <button className="button is-danger" type="button">Annuler</button>
-            </Link>
-          </div>
-        </div>
-      </article>
-    );
+  componentDidMount() {
+    document.title = "Admin / Récapitulatif création d'utilisateur";
   }
+
+    createUser = (e) => {
+      e.preventDefault();
+      const { user } = this.state;
+      Axios.post('http://localhost:4000/api/users ', user)
+        .then(() => this.setState(() => ({ redirectToReferrer: true })));
+    };
+
+    render() {
+      const { user } = this.props;
+      let roleDetail;
+      if (user.role === 'admin') roleDetail = 'Administrateur';
+      if (user.role === 'teacher') roleDetail = 'Formateur';
+      if (user.role === 'student') roleDetail = 'Eleve';
+
+      const { redirectToReferrer } = this.state;
+      if (redirectToReferrer === true) {
+        return <Redirect to="/home/admin" />;
+      }
+
+      return (
+        <article className="section box">
+          <h2 className="title is-2 is-spaced">Récapitulatif du formulaire d&lsquo;utilisateur</h2>
+          <section className="box">
+            <h2 className="title is-2 is-spaced">
+              <ul className="list" id="recapUserList">
+                <li id="lastName" className="list-item">
+                Nom :&nbsp;
+                  <span>
+                    { user.lastName }
+                  </span>
+                </li>
+                <li id="firstName" className="list-item">
+                Prénom :&nbsp;
+                  <span>
+                    { user.firstName }
+                  </span>
+                </li>
+                <li id="email" className="list-item">
+                Email :&nbsp;
+                  <span>
+                    { user.email }
+                  </span>
+                </li>
+                <li id="role" className="list-item">
+                Rôle :&nbsp;
+                  <span>
+                    { roleDetail }
+                  </span>
+                </li>
+              </ul>
+            </h2>
+          </section>
+          <div className="field is-grouped">
+            <div className="control">
+              <button type="submit" id="createButton" onClick={(e) => this.createUser(e)} className="button is-success">Créer</button>
+            </div>
+            <div className="control">
+              <a href="/home/admin/" id="cancelButton" className="button is-danger userProfileButton">Annuler</a>
+            </div>
+          </div>
+        </article>
+      );
+    }
 }
 
 export default RecapProgramForm;
