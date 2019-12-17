6ddacc853727057bfadd302de93761b50a70ab1b
Anne-Lise VANHOEGAERDEN <al.vanhoegaerden@gmail.com>
2019-12-17 13:46:18 +0100

    program details tests

--- src/components/programDetails/ProgramDetails.test.js
+++ src/components/programDetails/ProgramDetails.test.js
@@ -1,37 +1,33 @@
-// import ReactDOM from 'react-dom';
-// import renderer from 'react-test-renderer';
-// import React from 'react';
-// import Enzyme, { shallow } from 'enzyme';
-// import axios from 'axios';
-// import Adapter from 'enzyme-adapter-react-16';
-// import ProgramDetails from './ProgramDetails';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import axios from 'axios';
+import Adapter from 'enzyme-adapter-react-16';
+import ProgramDetails from './ProgramDetails';
 
-// Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
-// jest.mock('axios');
+jest.mock('axios');
 
-// describe('ProgramDetails component', () => {
-// it.only('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<ProgramDetails />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
-// it.only('matches snapshot', () => {
-//   const component = renderer.create(<ProgramDetails />);
-//   const tree = component.toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
-// it('contains a title "Bienvenue"', () => {
-//   const app = shallow(<ProgramDetails />);
-//   expect(app.containsMatchingElement(<h1>Bienvenue</h1>)).toEqual(true);
-// });
-// describe('when rendered', () => {
-//   it('should fetch a promotion', () => {
-//     const getSpy = jest.spyOn(axios, 'get');
-//     shallow(
-//       <ProgramDetails />,
-//     );
-//     expect(getSpy).toBeCalled();
-//   });
-// });
-// });
+describe('ProgramDetails component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ProgramDetails match={{ params: { id: 1 } }} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it('matches snapshot', () => {
+    const component = renderer.create(<ProgramDetails match={{ params: { id: 1 } }} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  describe('when rendered', () => {
+    it('should fetch a program details', () => {
+      const getSpy = jest.spyOn(axios, 'get');
+      shallow(
+        <ProgramDetails match={{ params: { id: 1 } }} />,
+      );
+      expect(getSpy).toBeCalled();
+    });
+  });
+});
---
+++ src/components/programDetails/__mocks__/axios.js
@@ -0,0 +1,56 @@
+
+module.exports = {
+  get: () => Promise.resolve({
+    data: {
+      id: 1,
+      title: 'Javascript',
+      type: 1,
+      content: [
+        {
+          id: 13,
+          title: 'module js',
+          type: 2,
+          content: [
+            {
+              id: 14,
+              title: 'sous module js',
+              type: 3,
+              content: [
+                {
+                  id: 15,
+                  title: 'sequence js',
+                  type: 4,
+                  content: [],
+                  createdAt: '2014-02-02T00:00:00.000Z',
+                  updatedAt: '2014-02-02T00:00:00.000Z',
+                },
+              ],
+              createdAt: '2014-02-02T00:00:00.000Z',
+              updatedAt: '2014-02-02T00:00:00.000Z',
+            },
+            {
+              id: 17,
+              title: 'sous module js 2',
+              type: 3,
+              content: [],
+              createdAt: '2014-02-02T00:00:00.000Z',
+              updatedAt: '2014-02-02T00:00:00.000Z',
+            },
+          ],
+          createdAt: '2014-02-02T00:00:00.000Z',
+          updatedAt: '2014-02-02T00:00:00.000Z',
+        },
+        {
+          id: 16,
+          title: 'module 2 js',
+          type: 2,
+          content: [],
+          createdAt: '2014-02-02T00:00:00.000Z',
+          updatedAt: '2014-02-02T00:00:00.000Z',
+        },
+      ],
+      createdAt: '2012-12-12T00:00:00.000Z',
+      updatedAt: '2012-12-12T00:00:00.000Z',
+    },
+  }),
+};
